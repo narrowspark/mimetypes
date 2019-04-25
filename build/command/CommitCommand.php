5e47e79ddc7e7ef8dd57f37cd53e682be1f84e8d
prisis <d.bannert@anolilab.de>
2019-04-25 13:24:37 +0200

    feat: fixed commit command to use the last master version to check

--- build/command/CommitCommand.php
+++ build/command/CommitCommand.php
@@ -45,8 +45,11 @@ class CommitCommand extends AbstractCommand
     public function handle(): int
     {
         $mimeDbVersion = $this->yarnLock->getPackage('mime-db')->getVersion();
+        // Get the last master version to check if the package should be upgraded.
+        $masterPackageJson = \file_get_contents('https://raw.githubusercontent.com/narrowspark/mimetypes/master/package.json');
+        $masterPackageArray = \json_decode($masterPackageJson, true);
 
-        if ($mimeDbVersion === MimeTypesList::MIME_DB_VERSION) {
+        if ($mimeDbVersion === $masterPackageArray['dependencies']['mime-db']) {
             $this->info('Nothing to update.');
 
             return 0;
@@ -54,7 +57,7 @@ class CommitCommand extends AbstractCommand
 
         $this->info('Making a commit to narrowspark/mimetypes.');
 
-        $filesToCommit = ' -o ' . $this->rootPath . \DIRECTORY_SEPARATOR . 'src' . \DIRECTORY_SEPARATOR . 'MimeTypesList.php  -o ' . $this->rootPath . \DIRECTORY_SEPARATOR . 'yarn.lock';
+        $filesToCommit = ' -o ' . $this->rootPath . \DIRECTORY_SEPARATOR . 'src' . \DIRECTORY_SEPARATOR . 'MimeTypesList.php  -o ' . $this->rootPath . \DIRECTORY_SEPARATOR . 'package.json';
 
         $gitCommitProcess = new Process(
             'git commit -m "Automatically updated on ' . (new \DateTimeImmutable('now'))->format(\DateTimeImmutable::RFC7231) . '"' . $filesToCommit
@@ -100,7 +103,7 @@ class CommitCommand extends AbstractCommand
             return 1;
         }
 
-        $this->info($gitCreateTagProcess->getOutput());
+        $this->info($tag = $gitCreateTagProcess->getOutput());
 
         $gitPushTagProcess = new Process(\sprintf('git push origin %s --quiet', $tag));
         $gitPushTagProcess->run();
