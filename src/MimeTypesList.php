dbeaa3d3f582d947e865949230f27a531db0bac4
prisis <d.bannert@anolilab.de>
2019-09-12 09:42:27 +0000

    Automatically updated on Thu, 12 Sep 2019 09:42:27 GMT

--- src/MimeTypesList.php
+++ src/MimeTypesList.php
@@ -16,7 +16,7 @@ class MimeTypesList
     /**
      * A map of file extensions and their mime types.
      *
-     * Last generated: Tue, 10 Sep 2019 21:39:01 GMT
+     * Last generated: Thu, 12 Sep 2019 09:42:27 GMT
      *
      * @see http://www.iana.org/assignments/media-types/media-types.xhtml
      * @see http://svn.apache.org/repos/asf/httpd/httpd/trunk/docs/conf/mime.types
